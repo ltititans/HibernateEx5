5eef0596dca6f2fcfb10c58ef71de02dc05124d6
shan <i.v.shanmukh@gmail.com>
2019-11-06 16:25:16 +0530

    updater1

--- hibernate_Proj/src/main/java/com/lti/hibernate_ex/hibernate_Proj/App.java
+++ hibernate_Proj/src/main/java/com/lti/hibernate_ex/hibernate_Proj/App.java
@@ -1,5 +1,10 @@
 package com.lti.hibernate_ex.hibernate_Proj;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    Configuration configuration=new Configuration().configure();
+    StandardServiceRegistryBuilder builder=new StandardServiceRegistryBuilder();
+    SessionFactory factory=configuration.buildSessionFactory(builder.build());
+    Session session=factory.openSession();
+    Employee employee=new Employee();
+    employee.setEmpName("Sindhura");
+   employee.setBranch("Mumbai");
+   session.beginTransaction();
+   session.save(employee);
+   session.getTransaction().commit();
+    
     }
 }
